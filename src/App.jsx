5b037c3ac7279698ccef89d105ecd48c770a159c
Oghenetega Esedere <arcteggzz@gmail.com>
2023-08-31 23:11:23 +0100

    feat: added the single card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import "./App.css";
 import ItemDisplay from "./components/ItemDisplay";
 
@@ -7,10 +7,11 @@ import ItemInput from "./components/ItemInput";
 
 function App() {
   const itemList = useSelector((state) => state.items.value);
+
   return (
     <div className="w-11/12 xl:w-customWidth my-10 mx-auto">
-      <ItemInput itemList={itemList}/>
-      <ItemDisplay itemList={itemList}/>
+      <ItemInput itemList={itemList} />
+      <ItemDisplay />
     </div>
   );
 }
--- src/ItemList.js
+++ src/ItemList.js
@@ -1,10 +1,15 @@
 import image from "./assets/item-image.jpg";
 export const ItemsData = [
-    {
-        id: 1,
-        name: 'Jersey',
-        image: image,
-        price: 6000,
-    },
-    
-]
+  {
+    id: 1,
+    name: "Jersey",
+    image: image,
+    price: 6000,
+  },
+  {
+    id: 2,
+    name: "Jite",
+    image: image,
+    price: 9000,
+  },
+];
